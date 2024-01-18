529b6d481f35668ddc007f798e3e8d24de2c448c
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-01-18 23:44:55 +0800

    first commit

---
+++ server/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+import { getVideoInfo } from './src/download.ts';
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.get('/', (_req: Request, res: Response) => {
+  return res.send('Tiktok downloader API');
+})
+app.get('/api', async (req: Request, res: Response) => {
+  const { url } = req.query;
+  try {
+    const data = await getVideoInfo(url);
+    res.send(data)
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal server error');
+  }
+})
+
+app.listen(port, () => console.log(`Server is listening on ${port}`))
+  
+export default app;
---
+++ server/src/download.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import spdy from 'spdy';
+
+const agent = spdy.createAgent({
+  host: 'www.tikwm.com',
+  port: 443,
+  rejectUnauthorized: false,
+});
+
+const instance = axios.create({
+  baseURL: 'https://www.tikwm.com',
+  httpAgent: agent,
+  headers: {
+    'accept': 'application/json, text/javascript, */*; q=0.01',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+  },
+  params: {
+    count: 12,
+    cursor: 0,
+    web: 1,
+    hd: 5
+  }
+});
+
+/*
+* @params {string} - url
+* @example 
+* // getVideoInfo('https://vt.tiktok.com/...')
+* @returns {object} 
+*/
+
+const getVideoInfo = async (url): Promise<any> => {
+  try {
+    const response = await instance.post('/api/', { url });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return { error: true };
+  }
+};
+
+export { getVideoInfo };
