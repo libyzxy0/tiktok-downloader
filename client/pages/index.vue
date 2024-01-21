b49a708d80d5fe8275bf9c3a4aebb93ea6dd7d18
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-01-21 16:26:24 +0800

    feat:Error handling alert and loading indecators

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -4,22 +4,43 @@
     description: "Download Tiktok Video with tikdown.click without watermark.", 
     ogTitle: "Tikdown: Download Tiktok Video without watermark.",
     ogDescription: "Download Tiktok Video with tikdown.click without watermark.",
-    ogImage: "https://tikdown.click/favicon.ico"
+    ogImage: "https://tikdown.click/favicon.ico", 
+    ogImageAlt: "Tikdown: Download Tiktok Video without watermark."
   })
   const url = ref("");
   const btnIsLoading = ref(false);
   const isError = ref(false);
+  const error = ref({});
   const downloaded = ref({});
+  const showError = (title, msg) => {
+    error.value.message = msg;
+    error.value.title = title;
+    alertModal.showModal();
+    setTimeout(() => {
+      alertModal.close();
+      error.value.message = "";
+      error.value.title = "";
+    }, 3000)
+  }
   const clickDownload = async () => {
   btnIsLoading.value = true;
+  if(!url.value) {
+    isError.value = true;
+    btnIsLoading.value = false;
+    setTimeout(() => {
+      isError.value = false;
+    }, 3000)
+    showError("Oppsss.. Error", "Missing tiktok url!");
+    return;
+  }
   try {
     const response = await fetch('https://api.tikdown.click/api?url=' + url.value);
-
     if (!response.ok) {
       isError.value = true;
       setTimeout(() => {
         isError.value = false;
       }, 3000)
+      showError("Error " + response.status, "Please check your video url!")
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
@@ -29,6 +50,7 @@
       setTimeout(() => {
         isError.value = false;
       }, 3000)
+      showError("Oppsss.. Error", "Please check your video url!")
     }
     btnIsLoading.value = false;
 
@@ -39,6 +61,7 @@
       setTimeout(() => {
         isError.value = false;
       }, 3000)
+    showError("Oppsss.. Error!", "Please check your video url!")
     console.error("Error during API request:", error);
     btnIsLoading.value = false;
   }
@@ -50,8 +73,17 @@ const downloadOnDevice = (type) => {
 };
 </script>
 <template>
+  <dialog id="alertModal" class="modal">
+  <div class="modal-box bg-gradient-to-tl from-slate-800 to-slate-900 bg-gradient-to-r shadow">
+    <h3 class="font-bold text-lg text-red-500">{{ error.title }}</h3>
+    <p class="py-4 text-white">{{ error.message }}</p>
+  </div>
+  <form method="dialog" class="modal-backdrop">
+    <button>close</button>
+  </form>
+</dialog>
   <dialog id="downloadDialog" class="modal">
-  <div class="modal-box bg-slate-700">
+  <div class="modal-box bg-gradient-to-tl from-slate-800 to-slate-900 bg-gradient-to-r shadow">
     <form method="dialog">
       <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-white">✕</button>
     </form>
@@ -63,20 +95,20 @@ const downloadOnDevice = (type) => {
           <source :src="downloaded?.id ? `https://www.tikwm.com/video/media/hdplay/${downloaded.id}.mp4` : ''">
         </video>
       </div>
-      <button @click="downloadOnDevice('hd')" class="btn bg-gradient-to-r from-purple-500 to-pink-500 py-3 w-[90%] border-none rounded-full shadow text-white">Download HD</button>
+      <button @click="downloadOnDevice('hd')" class="btn bg-gradient-to-r from-purple-500 to-pink-500 py-3 w-[90%] border-none rounded-full shadow text-white">Download HD (Beta)</button>
       <button @click="downloadOnDevice('nl')" class="btn bg-gradient-to-r from-purple-500 to-pink-500 py-3 w-[90%] border-none rounded-full shadow text-white mt-2">Download 720p</button>
-       <button @click="downloadOnDevice('wm')" class="btn bg-gradient-to-r from-purple-500 to-pink-500 py-3 w-[90%] border-none rounded-full shadow text-white mt-2">Download with watermark</button>
+       <button @click="downloadOnDevice('wm')" class="btn bg-gradient-to-r from-purple-500 to-pink-500 py-3 w-[90%] border-none rounded-full shadow text-white mt-2">Download with Watermark</button>
     </div>
   </div>
   </dialog>
   <div class="bg-gradient-to-tl from-slate-800 to-slate-900 bg-gradient-to-r w-full h-screen opacity-90 overflow-y-auto">
     <Navbar />
-    <h1 class="text-center text-purple-400 text-[30px] md:text-[40px] mx-4 mt-7 font-bold bg-gradient-to-r from-purple-500 via-red-400 to-pink-500 bg-clip-text text-transparent mt-[5rem]">Download Tiktok Video without watermark!</h1>
+    <h1 class="text-center text-purple-400 text-[30px] md:text-[40px] mx-4 mt-8 font-bold bg-gradient-to-r from-purple-500 via-red-400 to-pink-500 bg-clip-text text-transparent mt-[5rem]">Download Tiktok Video without watermark!</h1>
     <div class="flex flex-col justify-center items-center my-4">
       <div :class="isError ? 'bg-red-500' : 'bg-gradient-to-r from-purple-500 via-red-500 to-pink-500'" class="h-12 w-[85%] md:w-[60%] rounded-full p-[1.5px]">
         <input v-model="url" type="text" class="outline-none w-full h-full rounded-full bg-slate-700 px-4 py-2 text-white font-medium" placeholder="Enter tiktok url.">
       </div>
-      <button class="border-none bg-gradient-to-r from-purple-500 via-pink-500 to-red-400 w-[85%] md:w-[60%] py-3 rounded-full mt-4 shadow text-white font-medium btn text-[16px] shadow" @click="clickDownload">{{ btnIsLoading ? 'Downloading' : 'Download' }}</button>
+      <button class="border-none bg-gradient-to-r from-purple-500 via-pink-500 to-red-400 w-[85%] md:w-[60%] py-3 rounded-full mt-4 shadow text-white font-medium btn text-[16px] shadow" @click="clickDownload"><span v-if="btnIsLoading" class="loading loading-dots loading-md"></span><p v-else>Download</p></button>
     </div>
     <Line />
     <HowToDownload />
